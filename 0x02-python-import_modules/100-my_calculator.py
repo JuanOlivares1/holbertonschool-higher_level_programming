d1017288b4a4638f81915aed8484dc7351dc8156
Juan Olivares <jdop2000@gmail.com>
2020-05-01 01:45:59 +0000

    calculator refactor

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,23 +2,23 @@
 if __name__ == "__main__":
     from sys import argv
     from calculator_1 import add, sub, mul, div
-    argnum = len(argv) - 1
-    if argnum != 3:
+    argnum = len(argv)
+    if argnum != 4:
         print("Usage: {:s} <a> <operator> <b>".format(argv[0]))
         exit(1)
     a = int(argv[1])
     op = argv[2]
     b = int(argv[3])
     if op == "+":
-        foo = add
+        foo = add(a , b)
     elif op == "-":
-        foo = sub
+        foo = sub(a, b)
     elif op == "*":
-        foo = mul
+        foo = mul(a, b)
     elif op == "/":
-        foo = div
+        foo = div(a, b)
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
     string = "{:d} {} {:d} = ".format(a, op, b)
-    print("{:s} {:d}".format(string, foo(a, b)))
+    print("{:s} {:d}".format(string, foo))
