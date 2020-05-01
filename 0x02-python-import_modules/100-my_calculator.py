3b1e7984ed7e39adff53cdec68c0b2e90f2738b2
Juan Olivares <jdop2000@gmail.com>
2020-05-01 01:39:11 +0000

    refactoring calculator - fast alpha

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -6,11 +6,19 @@ if __name__ == "__main__":
     if argnum != 3:
         print("Usage: {:s} <a> <operator> <b>".format(argv[0]))
         exit(1)
-    a, op, b = int(argv[1]), argv[2], int(argv[3])
-    if op not in "+-*/":
+    a = int(argv[1])
+    op = argv[2]
+    b = int(argv[3])
+    if op == "+":
+        foo = add
+    elif op == "-":
+        foo = sub
+    elif op == "*":
+        foo = mul
+    elif op == "/":
+        foo = div
+    else:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
     string = "{:d} {} {:d} = ".format(a, op, b)
-    for op2, foo in [('+', add), ('-', sub), ('*', mul), ('/', div)]:
-        if op == op2:
-            print("{:s} {:d}".format(string, foo(a, b)))
+    print("{:s} {:d}".format(string, foo(a, b)))
