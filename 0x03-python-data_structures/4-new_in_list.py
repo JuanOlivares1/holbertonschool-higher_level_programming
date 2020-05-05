0e6ac00f0e20609345d12446b7418d3ada087768
Juan Olivares <jdop2000@gmail.com>
2020-05-05 21:18:28 +0000

    py function - replace a item of a list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    cpy = my_list[:]
-    if idx < 0 or idx >= len(my_list):
+    cpy = my_list.copy()
+    if idx < 0 or idx > len(cpy):
         return cpy
     cpy.insert(idx, element)
     return cpy
