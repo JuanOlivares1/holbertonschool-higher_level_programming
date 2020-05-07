faba59bf446afeeebfe4c0c4569aa6d616030c5c
Juan Olivares <jdop2000@gmail.com>
2020-05-07 22:12:25 +0000

    py function - adds all unique numbers of a list

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
+    res = 0
     if mylist:
         numbers = set(my_list)
         for i in numbers:
             res += i
-        return res
+    return res
