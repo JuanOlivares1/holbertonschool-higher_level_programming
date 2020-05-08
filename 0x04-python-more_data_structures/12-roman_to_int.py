54457ed24fe014558be8c688264f08e22274e7c0
Juan Olivares <jdop2000@gmail.com>
2020-05-08 04:23:58 +0000

    roman to int

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     if roman_string:
-        values = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+        values = {'M': 1000, 'D': 500,
+                  'C': 100, 'L': 50,
+                  'X': 10, 'V': 5, 'I': 1}
         res, temp = 0, 0
         for letter in reversed(roman_string):
             val = values[letter]
