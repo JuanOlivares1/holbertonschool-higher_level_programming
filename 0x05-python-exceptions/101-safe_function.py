93dc6f7b35b93852c9bdf7d7a8296717dab74a80
Juan Olivares <jdop2000@gmail.com>
2020-05-18 23:13:30 +0000

    py fuction - excecutes other function handling errors

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -3,7 +3,7 @@ def safe_function(fct, *args):
     from sys import stderr
     res = None
     try:
-        res = fct(args[0], args[1])
+        res = fct(*args)
     except ZeroDivisionError as zde:
         print("Exception: {}".format(zde), file=stderr)
     except IndexError as ie:
