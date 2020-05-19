41ed0aa1f8c75ccb87ee41fa293fdccba14a4263
Juan Olivares <jdop2000@gmail.com>
2020-05-19 23:16:19 +0000

    py module - square class - defines a square by given size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Python module - Cwrite a class Square that defines a square by size
+"""
+
+
+class Square:
+    """Square
+    """
+    def __init__(self, size):
+        """__init__ constructor
+
+        Args:
+            size (int): square size
+        """
+        self.__size = size
