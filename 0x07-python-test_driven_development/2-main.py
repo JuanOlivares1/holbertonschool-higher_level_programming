5fe660b1ee4f7ef30c66b4faad86f6bfc1b17948
Juan Olivares <jdop2000@gmail.com>
2020-05-22 23:02:29 +0000

    adding doctest of 'matrix_divided' function

--- 0x07-python-test_driven_development/2-main.py
+++ 0x07-python-test_driven_development/2-main.py
@@ -2,9 +2,8 @@
 matrix_divided = __import__('2-matrix_divided').matrix_divided
 
 matrix = [
-    [1, 2, 3],
-    [4, 5, 6]
+    [3, 6, 9],
+    [12, 15, 18]
 ]
-matrix_divided(matrix, 2, "test", 13.321)
 print(matrix_divided(matrix, 3))
 print(matrix)
--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,3 +1,26 @@
 #!/usr/bin/python3
 def matrix_divided(matrix, div):
-    pass
+    if type(matrix) == list:
+        new = []
+        for row in matrix:
+            new_row = []
+            if type(row) != list:
+                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+            elif len(row) == len(matrix[0]):
+                if type(div) not in [int, float]:
+                    raise TypeError("div must be a number")
+                    return None
+                if div == 0:
+                    raise ZeroDivisionError("division by zero")
+                    return None
+                for i in row:
+                    if type(i) not in [int, float]:
+                        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                    else:
+                        new_row.append(round(i / div, 2))
+                new.append(new_row)
+            else:
+                raise TypeError("Each row of the matrix must have the same size")
+    else:
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    return new
