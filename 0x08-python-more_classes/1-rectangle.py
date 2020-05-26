d0b4d084baf3126884264bb60ddafdbba98a10bb
Juan Olivares <jdop2000@gmail.com>
2020-05-26 05:10:39 +0000

    refactoring 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -26,14 +26,16 @@ class Rectangle:
     def width(self, width):
         if type(width) != int:
             raise TypeError("width must be an integer")
-        if width < 0:
+        elif width < 0:
             raise ValueError("width must be >= 0")
-        self.__width = width
+        else:
+            self.__width = width
 
     @height.setter
     def height(self, height):
         if type(height) != int:
             raise TypeError("height must be an integer")
-        if height < 0:
+        elif height < 0:
             raise ValueError("height must be >= 0")
-        self.__height = height
+        else:
+            self.__height = height
