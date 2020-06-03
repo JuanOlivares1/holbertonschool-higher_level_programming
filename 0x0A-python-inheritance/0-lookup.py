207686ebb32627c99b3ddec5475801431cffe5cf
Juan Olivares <jdop2000@gmail.com>
2020-06-03 15:39:43 +0000

    adding pep8

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
+"""Module
+"""
+
+
 def lookup(obj):
+    """lookup function - return object dir
+    Args:
+        obj (obj): object to take dir from
+    """
     return dir(obj)
