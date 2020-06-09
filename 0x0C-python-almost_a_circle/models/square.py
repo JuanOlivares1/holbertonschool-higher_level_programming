d71ed4b580fbcc33f0a5c0ea8311b1dd0dd64638
Juan Olivares <jdop2000@gmail.com>
2020-06-09 19:40:11 +0000

    square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -13,10 +13,26 @@ class Square(Rectangle):
             y (int)
         """
         super().__init__(size, size, x, y, id)
+        self.size = size
+
+    @property
+    def size(self):
+        """size getter
+        Retrun: size"""
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        """size setter
+        Args:
+            size (int)"""
+        self.width = size
+        self.height = size
+        self.__size = size
 
     def __str__(self):
         """graphic representation of rectangle
         Return: string"""
         return "[{}] ({}) {}/{} - {}".format(self.__class__.__name__,
                                              self.id, self.x, self.y,
-                                             self.width, self.height)
+                                             self.size)
