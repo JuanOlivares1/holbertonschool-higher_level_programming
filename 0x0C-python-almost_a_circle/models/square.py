421bea68c8441e289ca0797187fbf3ab34be6d18
Juan Olivares <jdop2000@gmail.com>
2020-06-09 19:15:17 +0000

    square

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Module - Square class"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Class Square"""
+    def __init__(self, size, x=0, y=0, id=None):
+        """cosntructor
+        Args:
+            size(int)
+            x (int)
+            y (int)
+        """
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """graphic representation of rectangle
+        Return: string"""
+        return "[{}] ({}) {}/{} - {}".format(self.__class__.__name__,
+                                                self.id, self.x, self.y,
+                                                self.width, self.height)
