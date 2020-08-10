1c5718a0d4cc3d0a9946dbb3e49b6181bf73b724
Juan Olivares <jdop2000@gmail.com>
2020-08-10 04:19:43 +0000

    reformating

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -2,13 +2,12 @@
 """Script - lists all states from the database hbtn_0e_0_usa"""
 import sys
 from model_state import Base, State
+from model_city import City
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_city import City
 
 
 if __name__ == "__main__":
-
     engine = create_engine('mysql://{}:{}@{}:{}/{}'.format(
                sys.argv[1], sys.argv[2], 'localhost', 3306, sys.argv[3]))
 
@@ -20,3 +19,5 @@ if __name__ == "__main__":
 
     for ctiy, state in table:
         print("{}: ({}) {}".format(state.name, ctiy.id, ctiy.name))
+
+    session.close()
