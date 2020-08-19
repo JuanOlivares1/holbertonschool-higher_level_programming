3c9559c8fbdbd2cf34061b8199c0da2f87e94e0c
Juan Olivares <jdop2000@gmail.com>
2020-08-19 18:04:06 +0000

    6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -7,4 +7,5 @@ def find_peak(list_of_integers):
     if not len(list_of_integers):
         return None
     else:
-        return max(list_of_integers)
+        list_of_integers.sort()
+        return list_of_integers[-1]
