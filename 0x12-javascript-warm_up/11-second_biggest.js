65a82ca779b1b26d00a8592f78ab5ef2ea799236
Juan Olivares <jdop2000@gmail.com>
2020-09-15 21:40:07 +0000

    last try

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,9 +2,9 @@
 const argv = process.argv.slice(2);
 let max;
 
-if (Number(argv[1])) {
-  max = argv.sort(function (a, b) { return Number(b) - Number(a); })[0];
+if (argv[1]) {
+  max = argv.sort(function (a, b) { return b - a; })[0];
 } else {
   max = '0';
 }
-console.log(Number(max));
+console.log(max);
