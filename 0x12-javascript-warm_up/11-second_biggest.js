6bad146012cff67432f3928ba0bece473a3c6a21
Juan Olivares <jdop2000@gmail.com>
2020-09-15 21:26:23 +0000

    JS - bigest on array

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const argv = process.argv.slice(2);
+let max;
+
+if (argv[0]) {
+  argv.sort(function (a, b) { return Number(b) - Number(a); });
+  max = argv[0];
+} else {
+  max = 0;
+}
+console.log(max);
