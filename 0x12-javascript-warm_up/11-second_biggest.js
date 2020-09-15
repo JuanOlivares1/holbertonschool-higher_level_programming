ebcc13476318420b56607d0853b242181147b627
Juan Olivares <jdop2000@gmail.com>
2020-09-15 21:41:36 +0000

    last try

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,7 +3,7 @@ const argv = process.argv.slice(2);
 let max;
 
 if (argv[1]) {
-  max = argv.sort(function (a, b) { return b - a; })[0];
+  max = argv.sort(function (a, b) { return b - a; })[1];
 } else {
   max = '0';
 }
