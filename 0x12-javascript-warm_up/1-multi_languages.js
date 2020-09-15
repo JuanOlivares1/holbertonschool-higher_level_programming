bba0cba2bf756087616389cc5cc9a75b4303d550
Juan Olivares <jdop2000@gmail.com>
2020-09-15 20:22:01 +0000

    refactoring excersice

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,3 @@
 #!/usr/bin/node
-const c = 'C is fun';
-const py = 'Python is cool';
-const js = 'Javascript is amazing';
-console.log(c + '\n' + py + '\n' + js);
+const languages = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+console.log(languages[0] + '\n' + languages[1] + '\n' + languages[2]);
